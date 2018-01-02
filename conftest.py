f3891c8f9754d91f80e6f0a4f92ee023380b70ca
Alexey Panashchenko <alpa@ciklum.com>
2018-01-02 16:11:15 +0200

    Fix chromefriver

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
@@ -10,11 +11,13 @@ def url():
 
 @pytest.yield_fixture(scope='session', name='driver')
 def driver_factory(url):
+    chromedriver = "/home/ubuntu/app/chromedriver"
+    os.environ["webdriver.chrome.driver"] = chromedriver
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     # _driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
     # _driver = webdriver.Chrome('/home/ubuntu/app/chromedriver', chrome_options=chrome_options)
-    _driver = webdriver.Chrome()
+    _driver = webdriver.Chrome(chromedriver)
     _driver.get(url)
     yield _driver
     _driver.quit()
