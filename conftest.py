3136975a49a09b7204debb6660e6c6e21f9a5fd0
Alexey Panashchenko <alpa@ciklum.com>
2018-01-02 17:00:17 +0200

    Add options to chrome

--- conftest.py
+++ conftest.py
@@ -13,8 +13,8 @@ def driver_factory(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     # _driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
-    _driver = webdriver.Chrome(chrome_options=chrome_options)
-    # _driver = webdriver.Chrome('/home/ubuntu/app/chromedriver', chrome_options=chrome_options)
+    # _driver = webdriver.Chrome(chrome_options=chrome_options)
+    _driver = webdriver.Chrome('/home/ubuntu/app/chromedriver', chrome_options=chrome_options)
     _driver.get(url)
     yield _driver
     _driver.quit()
