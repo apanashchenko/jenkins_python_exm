9f2056413a1a5c75f8f64de30455b4b53b832081
Alexey Panashchenko <alpa@ciklum.com>
2018-01-03 14:26:28 +0200

    Trigger test

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def url():
 def driver_factory(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    _driver = webdriver.Chrome('/home/ubuntu/chromedriver', chrome_options=chrome_options)
+    _driver = webdriver.Chrome('/home/ubuntu/chromedriver',chrome_options=chrome_options)
     _driver.get(url)
     yield _driver
     _driver.quit()
