c5aed4ed8f6e3c77c0928f973f1afb8d6322e920
Alexey Panashchenko <alpa@ciklum.com>
2018-01-03 14:16:54 +0200

    Trigger test

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 
@@ -12,10 +11,7 @@ def url():
 def driver_factory(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--headless')
     # _driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
-    # _driver = webdriver.Chrome(chrome_options=chrome_options)
     _driver = webdriver.Chrome('/home/ubuntu/chromedriver', chrome_options=chrome_options)
     _driver.get(url)
     yield _driver
