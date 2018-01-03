afe4593e5fd272e22b9d16bfb29568b74f15c3b9
Alexey Panashchenko <alpa@ciklum.com>
2018-01-03 13:32:27 +0200

    Disable healess chrome

--- conftest.py
+++ conftest.py
@@ -12,12 +12,11 @@ def url():
 def driver_factory(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--headless')
     # _driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
     # _driver = webdriver.Chrome(chrome_options=chrome_options)
     _driver = webdriver.Chrome('/home/ubuntu/chromedriver', chrome_options=chrome_options)
-    _driver.switch_to_alert().accept()
     _driver.get(url)
     yield _driver
     _driver.quit()
